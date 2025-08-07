b394b3e1cbd9baea9e0a0f6b7f2d3ef41dac9e78
Ruth <ruthleeyuxin@gmail.com>
2025-08-07 09:17:09 -0700

    hoisting exercise

--- time-travel.js
+++ time-travel.js
@@ -8,14 +8,21 @@ destination = "Medieval Europe";
 console.log(`New destination: ${destination}`);
 /* Task 3: Declare a Constant Travel Date */
 // TODO: Use `const` to declare a variable named `travelDate` and set it to `"2024-03-15"`. Try to change the `travelDate` to another value and observe and explain what happens as a comment.
+const travelDate = "2024-03-15";
+// travelDate = "2025-03-15";
+console.log(`travel date: ${travelDate}`);
+
 /*
- * Observations:
- * TODO: Explain here.
+ * Observations: type error happened when console.log
+ * TODO: Explain here: const cannot reasign or redeclare
  */
 
 /* Task 4: Experiment with Variable Hoisting */
 // TODO: Before declaring any variable, try to print a variable named `timeMachineModel` to the console. Then, declare `timeMachineModel` using `var` and assign it the value `"T-800"`. Observe and explain what happens as a comment.
+console.log(timeMachineModel);
+var timeMachineModel = "T-800";
+
 /*
- * Observations:
- * TODO: Explain here.
+ * Observations:the console.log output is undefined
+ * TODO: keyword var is not avaliable for hoisting
  */
