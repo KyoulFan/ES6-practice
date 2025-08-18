25c75ef62f9f25d50e995981a1ff1aecab530a28
Ruth <ruthleeyuxin@gmail.com>
2025-08-17 18:34:45 -0700

    add map practice

---
+++ users.js
@@ -0,0 +1,13 @@
+const users = [
+  { firstName: "Alice", lastName: "Johnson", points: 120 },
+  { firstName: "Bob", lastName: "Smith", points: 99 },
+  { firstName: "Charlie", lastName: "Brown", points: 180 },
+];
+
+const newUsers = users.map(function (users) {
+  return {
+    fullName: `${users.firstName} ${users.lastName}`,
+    membershipStatus: points > 100 ? "Premium" : "Standard",
+  };
+});
+console.log(newUsers);
