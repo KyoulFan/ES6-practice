0777354a26b9b5b790edf6cbb054b5104bb61fd4
Ruth <ruthleeyuxin@gmail.com>
2025-08-26 08:12:34 -0700

    upload tree.js

--- data_structure_exercise/dsa-trees/tree.js
+++ data_structure_exercise/dsa-trees/tree.js
@@ -15,14 +15,14 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    if(!this.root) return 0;
+    if (!this.root) return 0;
 
     let total = this.root.val;
-    function sumCount(node){
-      for(let child of node.children){
+    function sumCount(node) {
+      for (let child of node.children) {
         total += child.val;
         //use recursion to traversing
-        if(child.children.length>0){
+        if (child.children.length > 0) {
           sumCount(child);
         }
       }
@@ -35,14 +35,42 @@ class Tree {
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
-
+    if (!this.root) return 0;
+    let evenNum = this.root.val % 2 === 0 ? 1 : 0;
+    function evenFind(node) {
+      for (let child of children) {
+        {
+          if (child.val % 2 === 0) {
+            evenNum++;
+          }
+          if (child.children.length > 0) {
+            evenFind(child);
+          }
+        }
+      }
+    }
+    evenFind(this.root);
+    return evenNum;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
-
+    if (!this.root) return 0;
+    let countGreater = this.root.val > lowerBound ? 1 : 0;
+    function findGreater() {
+      for (let child of children) {
+        if (child > lowerBound) {
+          countGreater++;
+        }
+        if (child.children.length > 0) {
+          findGreater(child);
+        }
+      }
+    }
+    findGreater(this.root);
+    return countGreater;
   }
 }
 
